06bbd5d8e03fa8d6690d968cc8ac6d382cef39b6
barryvaneeden <b_vaneeden@icloud.com>
2025-06-09 13:37:21 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from modules import orders
+from modules import facturatie
 
 def main():
-    st.title("Voorraadbeheer Pro V15 - Orders Module")
-    orders.app()
+    st.title("Voorraadbeheer Pro V15 - Facturatie Module")
+    facturatie.app()
 
 if __name__ == "__main__":
     main()
